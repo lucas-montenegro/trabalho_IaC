8251234b6f39fc8719d26242feea77497ef876ed
lucas-montenegro <43447533+lucas-montenegro@users.noreply.github.com>
2018-09-21 19:40:41 -0300

    Update codigo.c

--- codigo.c
+++ codigo.c
@@ -1,11 +1,11 @@
-#include <unistd.h> 
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 int main (int argc, char *argv[], char *envp[]) {
-
+int i = 1; // contador de segundos
 int pid ; /* identificador de processo */
 
 pid = fork () ; /* replicacção do processo */
@@ -16,14 +16,26 @@ if ( pid < 0 ) { /* se o fork não funcionou */
 }
 else if( pid > 0 ) /* se sou o processo pai*/
 {
+	while(i <= 10) {
+		printf("%d\n", i);
+		i++;
+		sleep(1);
+	}
 	//TODO guarde a cada segundo o consumo de memória (em Kilobytes) e CPU (em porcentagem) do processo filho
 	//TODO após 10 segundos de execução, mate o proceso filho
 }
 else /* senão, sou o processo filho (pid == 0) */
 {
-	//TODO se argv[1] for igual a 'cpu', executar código com utilização intensa da UCP
-	//TODO se argv[1] for igual a 'cpu-mem', executar código com utilização intensa da UCP e da memória
+	if(strcmp(argv, "cpu") == 0) { //TODO se argv[1] for igual a 'cpu', executar código com utilização intensa da UCP
+		for(; ;) {
 
+		}
+	}
+	else if(strcmp(argv, "cpu-mem") == 0) { 	//TODO se argv[1] for igual a 'cpu-mem', executar código com utilização intensa da UCP e da memória
+		for(; ;) {
+			malloc(sizeof(100000));
+		}
+	}
 }
 perror ("Erro: ") ; /* execve nãoo funcionou */
 
