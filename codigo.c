80221d8bbf8f81d8861a0a7428067c41d399ebbb
lucas-montenegro <43447533+lucas-montenegro@users.noreply.github.com>
2018-09-30 21:40:13 -0300

    Update codigo.c

--- codigo.c
+++ codigo.c
@@ -1,3 +1,72 @@
+
+Save New Duplicate & Edit Just Text Twitter
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+37
+38
+39
+40
+41
+42
+43
+44
+45
+46
+47
+48
+49
+50
+51
+52
+53
+54
+55
+56
+57
+58
+59
+60
+61
+62
+63
+64
+65
+66
+67
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
@@ -23,9 +92,12 @@ void consumo_memoria() {
 int main (int argc, char *argv[], char *envp[]) {
 	int i = 1; // contador de segundos
 	int pid ; /* identificador de processo */
-
 	pid = fork () ; /* replicacção do processo */
-
+	char p1[10] = "ps -p";
+	char p2[20] = "-o %cpu";
+	char bash_cmd[256];
+	sprintf(bash_cmd,"%s %d %s", p1, pid, p2);
+	printf("%s\n", bash_cmd);
 	if ( pid < 0 ) { /* se o fork não funcionou */
 		perror ("Erro: ") ;
 		exit (-1) ; /* encerra o processo com código de erro -1 */
@@ -33,9 +105,8 @@ int main (int argc, char *argv[], char *envp[]) {
 	else if( pid > 0 ) /* se sou o processo pai*/
 	{
 		while(i <= 10) {
-			system("ps -p <pid> -o %cpu");
+			system(bash_cmd);
 			consumo_memoria();
-
 			i++;
 			sleep(1);
 		}
