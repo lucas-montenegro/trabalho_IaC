61247e8342de78eb3b9a14ef1dc200c74272b0fa
lucas-montenegro <43447533+lucas-montenegro@users.noreply.github.com>
2018-10-04 21:14:21 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,95 +1,72 @@
 #include <unistd.h>
-#include <sys/types.h>
 #include <sys/wait.h>
-#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
 
-void consumo_cpu(int pid) {
-	char p1[10] = "ps -p"; // primeira parte do comando
-	char p2[40] = "-o %cpu | grep -v %CPU"; // parte final do comando
-	char bash_cmd[256]; // vai receber o comando "completo" a partir da concatenacao
-
-	sprintf(bash_cmd,"%s %d %s", p1, pid, p2); // concatena a string para usar o pid
-
-	system(bash_cmd); // faz a chamada de sistema para calcular o uso da cpu
-
-	return;
+void cpu_usage(int pid) {
+    char c1[10] = "ps -p";                     /* first piece of command */
+    char c2[40] = "-o %cpu | grep -v %CPU";    /* final piece of command */
+    char bash_cmd[256];                        /* receives the "complete" command after concatenation */
+    sprintf(bash_cmd,"%s %d %s", c1, pid, c2); /* concatenates the string to include the pid */
+    system(bash_cmd);                          /* performs the system call to evaluate cpu usage */
 }
 
-void consumo_memoria(int pid) {
-	char m1[10] = "pmap"; // primeira parte do comando
-	char m2[40] = "| grep -i total | awk '{print $2}'"; // parte final do comando
-	char bash_cmd2[256]; // vai receber o comando "completo" a partir da concatenacao
-
-	sprintf(bash_cmd2,"%s %d %s", m1, pid, m2); // concatena a string para usar o pid
-
-	system(bash_cmd2); // faz a chamada de sistema para calcular o uso da cpu
-
-	return;
+void mem_usage(int pid) {
+    char m1[10] = "pmap";                               /* first piece of command */
+    char m2[40] = "| grep -i total | awk '{print $2}'"; /* final piece of command */
+    char bash_cmd2[256];                                /* receives the "complete" command after concatenation */
+    sprintf(bash_cmd2,"%s %d %s", m1, pid, m2);         /* concatenates the string to include the pid */
+    system(bash_cmd2);                                  /* performs the system call to evaluate memory usage */
 }
 
 int main (int argc, char *argv[], char *envp[]) {
-	int i = 1; // contador de segundos
-	int pid ; /* identificador de processo */
-	pid = fork () ; /* replicacção do processo */
-	
-	if ( pid < 0 ) { /* se o fork não funcionou */
-		perror ("Erro: ") ;
-		exit (-1) ; /* encerra o processo com código de erro -1 */
-	}
-	else if( pid > 0 ) /* se sou o processo pai*/
-	{
-		while(i <= 10) {
-			printf("Consumo da CPU(em %%):\n");
-			consumo_cpu(pid); // a funcao retorna em % o consumo da cpu pelo processo filho
+    int pid = fork(); /* creates identical child process and returns its id */
+    int i = 1; /* seconds counter */
 
-			if(strcmp(argv[1], "ucp-mem") == 0)
-			{
-				printf("Consumo de Memoria(em Kilobytes):\n"); 
-				consumo_memoria(pid); // a funcao retorna em kilobytes a memoria usada pelo processo filho
-			}
+    if ( pid < 0 ) { /* if fork was unsuccessful */
+        perror ("Error: ") ;
+        exit (-1) ;
+    }
+    else if( pid > 0 ) /* parent process instance */
+    {
+        while(i <= 10) {
+            printf("CPU consumption (in %%):\n");
+            cpu_usage(pid); /* returns child process's cpu usage in percentage */
 
-			printf("-----------------------------------\n");
-			i++; // contador 
-			sleep(1); // funcao para contar os segundos
-		}
+            if(strcmp(argv[1], "ucp-mem") == 0) /* cpu and memory intensive */
+            {
+                printf("Memory consumption (in Kilobytes):\n");
+                mem_usage(pid); /* returns child process's memory usage in KB */
+            }
+            printf("-----------------------------------\n");
+            i++;
+            sleep(1); /* puts parent process on hold for 1 second */
+        }
+        kill(pid, SIGKILL); /* kills child process */
+    }
+    else /* child process instance */
+    {
+        if(strcmp(argv[1], "ucp") == 0) { /* cpu intensive */
+            for(;;) {}
+        }
+        else if(strcmp(argv[1], "ucp-mem") == 0) {  /* cpu and memory intensive */
+            clock_t start_t, end_t, total_t;
 
-		printf ("Tchau !\n"); 
+            start_t = clock(); /* "cronometer's" start */
+            for(;;)
+            {
+                end_t = clock(); /* "cronometer's" end in each iteration */
+                total_t = (double)(end_t - start_t) / CLOCKS_PER_SEC; /* total of seconds passed */
 
-		kill(pid, SIGKILL); // mata o processo filho
-		
-	//TODO guarde a cada segundo o consumo de memória (em Kilobytes) e CPU (em porcentagem) do processo filho
-	//TODO após 10 segundos de execução, mate o proceso filho
-	}
-	else /* senão, sou o processo filho (pid == 0) */
-	{
-	if(strcmp(argv[1], "ucp") == 0) { //TODO se argv[1] for igual a 'cpu', executar código com utilização intensa da UCP
-		for(; ;) {
+                if (total_t >= 0 && total_t <= 10)
+                {
+                    malloc(10 * sizeof(int));
+                }
+            }
+        }
+    }
 
-		}
-	}
-	else if(strcmp(argv[1], "ucp-mem") == 0) { 	//TODO se argv[1] for igual a 'cpu-mem', executar código com utilização intensa da UCP e da memória
-		//int begin = time(NULL), end, i;
-		clock_t start_t, end_t, total_t;
-		int i;
-		
-		start_t = clock(); //começo do "cronômetro"
-		while(1)
-		{	
-			end_t = clock(); //fim do "cronômetro" a cada iteração
-			total_t = (double)(end_t - start_t) / CLOCKS_PER_SEC; //total de segundos passados
-
-			if (total_t == 0.0000 || total_t == 1.0000 || total_t == 2.0000 || total_t == 3.0000 || total_t == 4.0000 || total_t == 5.0000 || total_t == 6.0000 || total_t == 7.0000 || total_t == 8.0000 || total_t == 9.0000)
-			{
-				malloc(10 * sizeof(100000));
-			}
-		}	
-	}
-	perror ("Erro: "); /* execve nãoo funcionou */
-
-	exit(0) ; /* encerra o processo com sucesso (código 0) */
-	}
+    return 0;
 }
